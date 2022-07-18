a5bade2d4a2e5acf156fdaba0bb8e113484cff2e
Sriram Krishnan <sriramk1986@gmail.com>
2022-07-18 23:15:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
 
-function App() {
+import './App.css'
+import Movie from "./Movie"
+
+import SearchIcon from './search.svg'
+const API_URL = "http://www.omdbapi.com?apikey=8474d9a6"
+
+
+
+const App = () => {
+  const [title, setTitle] = useState("")
+  const [movies, setMovies] = useState([])
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json()
+    setMovies(data.Search)
+    console.log(data.Search)
+  }
+
+  useEffect(() => {
+    searchMovies('batman')
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input 
+          placeholder="" 
+          value={title} 
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        
+        <img 
+          src={SearchIcon} 
+          alt="search" 
+          onClick={() => {searchMovies(title)}} 
+        />
+      </div>
+
+      { movies.length > 0 ? (
+        <div className="container">
+          {movies.map(movie => (
+            <Movie key={movie.imdbID} movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )
+        
+      }
+      
+      
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Movie.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Movie = ({ movie }) => {
+  return (
+    <div className="movie">
+      <div>
+        <p>{movie.Year}</p>
+      </div>
+
+      <div>
+        <img src={movie.Poster !== "N/A" ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.Title} />
+      </div>
+    </div>
+  )
+}
+
+export default Movie
